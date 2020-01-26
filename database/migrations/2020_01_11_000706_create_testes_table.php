607c5aa80e6899119ad13cccebf27bb1e20de4e5
FelipeLeonardo98 <felipeferrazleonardo98@gmail.com>
2020-01-26 17:00:44 -0300

    Second Commit

---
+++ database/migrations/2020_01_11_000706_create_testes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTestesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('testes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->string('senha');
+            $table->double('valor', 5,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('testes');
+    }
+}
